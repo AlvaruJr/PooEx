b759a5e8d6e3bbfd454b25d9117760dbc12e474f
AlvaruJr <alvarujr.dev@gmail.com>
2025-02-25 22:45:58 -0300

    feat: implementar classe Esfera com cálculo de volume e área

--- src/Esfera.java
+++ src/Esfera.java
@@ -1,24 +1,24 @@
 public class Esfera {
     private float raio;
 
+    public Esfera(float raio) {
+        this.raio = raio;
+    }
 
     public void setRaio(float r) {
         this.raio = r;
     }
 
-
     public float calcularVolume() {
         return (float) ((4.0 / 3.0) * Math.PI * Math.pow(raio, 3));
     }
-
 
     public float calcularArea() {
         return (float) (4 * Math.PI * Math.pow(raio, 2));
     }
 
     public static void main(String[] args) {
-        Esfera esfera = new Esfera();
-        esfera.setRaio(5);
+        Esfera esfera = new Esfera(5);
         System.out.println("Volume da Esfera: " + esfera.calcularVolume());
         System.out.println("Área da Esfera: " + esfera.calcularArea());
     }

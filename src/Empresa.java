a17bc9d6faa9719fe830f3780390a010c3d4de0e
AlvaruJr <alvarujr.dev@gmail.com>
2025-02-25 23:05:23 -0300

    feat: implementar classe Empresa

---
+++ src/Empresa.java
@@ -0,0 +1,30 @@
+
+class Empresa {
+    private String razaoSocial;
+    private String cnpj;
+    private String fone;
+
+    public void setRazaoSocial(String rs) {
+        this.razaoSocial = rs;
+    }
+
+    public void setCNPJ(String c) {
+        this.cnpj = c;
+    }
+
+    public void setFone(String f) {
+        this.fone = f;
+    }
+
+    public String getRazaoSocial() {
+        return razaoSocial;
+    }
+
+    public String getCNPJ() {
+        return cnpj;
+    }
+
+    public String getFone() {
+        return fone;
+    }
+}
